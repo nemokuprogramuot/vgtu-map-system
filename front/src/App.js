11f105cbb21ea32f27744fabad0d0855e597b89f
Nilas Makuska <Ja5na5>
2025-03-24 13:30:04 +0200

    #VMS-14 created and modified dynamic routing

--- front/src/App.js
+++ front/src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // Use Routes instead of Switch
 import Main from "./Main";
 import ErrorPage from "./ErrorPage";
+import S1 from './S1';
+
 
 function App() {
   return (
@@ -10,6 +12,7 @@ function App() {
        
         <Routes> 
           <Route path="/" element={<Main />} /> 
+          <Route path="/S1" element={<S1 />} />
           <Route path="*" element={<ErrorPage />} />
         </Routes>
         
