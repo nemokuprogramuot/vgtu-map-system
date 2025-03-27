2137ad839ad27680ff43e62be27785ec678c14fa
rokas0504 <rokas.kamarauskas@stud.vilniustech.lt>
2025-03-27 16:45:55 +0200

    VMS-57 - added API key to env file, VMS-58 - Implemented a map to S1 page. VMS-56 - cofigured page updates on intial load, fixed S1 page. First base of google maps API done.

--- front/src/S1.js
+++ front/src/S1.js
@@ -1,8 +1,37 @@
-import React from "react";
+import React, { useState, useEffect, useCallback } from "react";
+import { GoogleMap } from "@react-google-maps/api";
 import zemelapis from "./photos/placeholder1.jpg"
 import pradine from "./photos/placeholder2.jpg"
 
+const containerStyle = {
+  width: "60%",
+  height: "800px",
+};
+
+const centerCordinates = {
+  lat: 54.722313,
+  lng: 25.337344,
+};
 const Page1 = () => {
+  const [map, setMap] = useState(null);
+
+  const onLoad = useCallback((mapInstance) => {
+    setMap(mapInstance);
+  }, []);
+
+  const onUnmount = useCallback(() => {
+    setMap(null);
+  }, []);
+
+  useEffect(() => {
+    if (map) {
+      const timeout = setTimeout(() => {
+        window.google.maps.event.trigger(map, "resize");
+        map.setCenter(centerCordinates);
+      }, 300);
+      return () => clearTimeout(timeout);
+    }
+  }, [map]);
 
   const styles = {
     container: {
@@ -49,15 +78,24 @@ const Page1 = () => {
     <div style={styles.container}>
       <h1 style={styles.header}>S1 Centriniai rūmai</h1>
       <h3 style={styles.subheader}>SRC</h3>
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={centerCordinates}
+        zoom={17}
+        onLoad={onLoad}
+        onUnmount={onUnmount}
+      >
+        {/* Add markers or other components if needed */}
+      </GoogleMap>
+      <p> CIA VIRTUALUS TURAS VIETOJ FT</p>
+      <p>|</p>
+      <p>v</p>
       <img
         src= {pradine}
         alt="Map of S1 Centriniai rūmai"
         style={styles.image}
       />
-      <p style={styles.description}>
-        <strong>Kaip patekti:</strong> Nuo centrinės aikštės (žr. žemėlapyje) pasukti
-        kairėn. Eiti prie stiklinio pastato.
-      </p>
+
       <p style={styles.description}>
         <strong>Navigacija:</strong>
       </p>
