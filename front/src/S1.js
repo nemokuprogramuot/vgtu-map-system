b659640ec3bd8babe962bb178619d119e502a662
Laurynas Butkus <lauris2661@gmail.com>
2025-03-28 23:46:55 +0200

    added markers to google maps api

--- front/src/S1.js
+++ front/src/S1.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useCallback } from "react";
-import { GoogleMap } from "@react-google-maps/api";
+import { GoogleMap, Marker} from "@react-google-maps/api";
 import zemelapis from "./photos/placeholder1.jpg"
 import pradine from "./photos/placeholder2.jpg"
 
@@ -12,6 +12,17 @@ const centerCordinates = {
   lat: 54.722313,
   lng: 25.337344,
 };
+
+const markers = [
+  { id: "S1", lat: 54.72274383397544, lng: 25.33764722315543 },
+  { id: "S2", lat: 54.72203499177293, lng: 25.33709238422004 },
+  { id: "S3", lat: 54.72250984691805, lng: 25.33618418529878 },
+  { id: "S4/5", lat: 54.72163036759537, lng: 25.337439235351688},
+  { id: "S6", lat: 54.7217984077603, lng: 25.336478409431674 },
+  { id: "S6", lat: 54.72178798668516, lng: 25.336187455101744 },
+  { id: "S7", lat: 54.72125260034498, lng: 25.33614685682314 }
+  
+];
 const Page1 = () => {
   const [map, setMap] = useState(null);
 
@@ -71,6 +82,7 @@ const Page1 = () => {
       alignItems: "center", 
       gap: "20px", 
     },
+    
    
   };
 
@@ -85,7 +97,18 @@ const Page1 = () => {
         onLoad={onLoad}
         onUnmount={onUnmount}
       >
-        {/* Add markers or other components if needed */}
+        {markers.map((marker) => (
+   <Marker 
+  key={marker.id}
+  position={{ lat: marker.lat, lng: marker.lng }}
+  label={{
+    text: marker.id,
+    fontSize: "15px",
+    fontWeight: "bold",
+    color: "black",
+  }}
+/>
+  ))}
       </GoogleMap>
       <p> CIA VIRTUALUS TURAS VIETOJ FT</p>
       <p>|</p>
