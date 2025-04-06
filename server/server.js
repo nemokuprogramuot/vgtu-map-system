e2f1669b301f9eada1c1cb5fd66b4986720f5d77
Laurynas Butkus <lauris2661@gmail.com>
2025-04-06 20:07:30 +0300

    #VMS 83 modified comment added symbol filtering

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const Data = require('./models/data') 
+require('dotenv').config();
+
 
 const app = express()
 
 //connect to mongodb
-const dbURI = "mongodb+srv://testas:testas123@tasker.rnrbv.mongodb.net/user-comments?retryWrites=true&w=majority&appName=Tasker" //data pridejau, gal istrint
+const dbURI = process.env.dbURI
 mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
 .then((result)=> app.listen(5000, ()=> {console.log("Server started on port 5000")}))
 .catch((err)=> console.log(err))
@@ -15,29 +17,19 @@ const cors = require("cors"); // laikinai
 app.use(cors());
 app.use(express.urlencoded({extended: true})); 
 
+function sanitizeInput(str) {
+    if (typeof str !== 'string') return '';
+    return str.replace(/['";\-\\/*]/g, '');
+}
+
 //tesnig purpose
-app.get("/add-data", (req,res) => {
+
     
-    const data = new Data({
-        title: "S1",
-        description: "SRA-I",
-        email:"pedro"
-    });
-    data.save()
-    .then((result) => {
-        res.send(result)
-    })
-    .catch((err) => {
-        console.log(err)
-    })
-
-})
-
 app.post("/add-comment", (req,res) => {
     const data = new Data({
-        title: req.body.title,
-        description: req.body.description,
-        email: req.body.email
+        title: sanitizeInput(req.body.title),
+        description: sanitizeInput(req.body.description),
+        email: sanitizeInput(req.body.email)
     });
 
     data.save()
